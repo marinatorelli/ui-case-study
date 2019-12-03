56fc1bf23298439def13897bc28000a86894ce17
Marina Torelli <45118915+marinatorelli@users.noreply.github.com>
2019-12-03 11:16:08 +0100

    events and home page updated - also sidebar update

---
+++ Events.js
@@ -0,0 +1,13 @@
+/* Function that receives as argument a column and opens its options menu */
+function showMenu(element) {
+	/* We get the children of the div column, the div with the menu has index 1 in this array */
+	var children = element.children;
+	/* We add/remove the class show, to change display from none to block and viceversa */
+	children[1].classList.toggle("hide");
+	var icon = children[0].children[0];
+	if (icon.innerHTML.localeCompare("keyboard_arrow_down")==0) {
+		icon.innerHTML = "keyboard_arrow_right";
+	} else {
+		icon.innerHTML = "keyboard_arrow_down";
+	}
+}
