c4ed0c5be784e5874e700cbcc0edc7bf44710c9c
carmenmera <56955131+carmenmera@users.noreply.github.com>
2019-12-04 12:14:45 +0100

    Added general style for popups

--- functions.js
+++ functions.js
@@ -1,11 +1,3 @@
-function share(){
-	/*Gets the modal*/
-	document.getElementById("share-popup").style.display="block";
-}
-function closeModal(){
-	document.getElementById("share-popu").style.display="none";
-}
-
 /* Function that receives as argument a column and opens its options menu */
 function showMenu(element) {
 	/* We get the children of the div column, the div with the menu has index 1 in this array */
@@ -18,4 +10,71 @@ function showMenu(element) {
 	} else {
 		icon.innerHTML = "keyboard_arrow_down";
 	}
+}
+
+/* Function that receives as argument a column and opens its options menu */
+function displayProjectsMenu(element) {
+	/* We get the children of the div column, the div with the menu has index 1 in this array */
+	var children = element.children;
+	/* We add/remove the class show, to change display from none to block and viceversa */
+	children[1].classList.toggle("show");
+}
+
+/* Function that receives as argument an element (task or column) and closes it */
+function closeElement(element) {
+    element.remove();
+}
+
+function complete(task) {
+	if (task.innerHTML.localeCompare("radio_button_unchecked")==0) {
+		task.innerHTML = "radio_button_checked";
+	} else {
+		task.innerHTML = "radio_button_unchecked";
+	}
+}
+
+$(document).ready(function() {
+    makeDrag();
+    console.log("after");
+});
+
+function makeDrag() {
+    $("body").sortable({
+        items: ".column",
+        axis: "x"
+    });
+    $(".column").sortable({
+        items: ".task",
+        connectWith: ".column"
+    });
+}
+
+function attend(attend) {
+	attend.className = "attending";
+	attend.innerHTML = "Attending";
+}
+
+function show_popup(id) {
+	var popup = document.getElementById(id);
+	popup.style.display = "flex";
+}
+
+/* Function that hides the popup to close it */
+function close_popup(popup) {
+    popup.style.display = "none";
+}
+
+function createLabel(popup) {
+	var newLabel = document.forms["label-form"]["label"].value;
+	var label = document.createElement("div");
+	label.classList.add("element");
+	var icon = document.createElement("i");
+	icon.classList.add('fas');
+	icon.classList.add('fa-tag')
+	label.appendChild(icon);
+	var p = document.createElement("p");
+	p.innerHTML = newLabel;
+	label.appendChild(p);
+	var menu = document.getElementById("labels").appendChild(label);
+	popup.style.display = "none";
 }
